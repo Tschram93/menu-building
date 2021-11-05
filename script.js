42ced563ea6bdef77cf3dc717f2c3b17aa7b10d5
TS93 <tschram93@yahoo.com>
2021-11-05 15:36:23 -0400

    Added listener on submit event; added function preventing page reload on submit

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+// Selectors
 const addItems = document.querySelector('.add-items');
 const itemsList = document.querySelector('.plates');
+// Empty Array where items will be stored
 const items = [];
+
+// Event Listener
+addItems.addEventListener('submit', addItem);
+
+// Functions
+// Adds Item to the list
+function addItem(e) {
+    e.preventDefault();
+
+
+}
